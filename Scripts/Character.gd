642e94af4125db5666aaf2c40658e719b2450640
Eduardo Kumagai Rocha <ekumagairocha@gmail.com>
2026-08-07 01:00:30 -0300

    Basic map and movement
    Added a simple map and a moveable character.

---
+++ Scripts/Character.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+@export var playerId : int = 0
+@export var team : int = 0
+@export var moveSpeed : float = 5.0
+@export var jumpVelocity : float = 10.0
+@export var fallSpeed : float = 15.0
+
+func _physics_process(delta : float) -> void:
+	if playerId > -1:
+		move_player_process(delta)
+
+func move_player_process(delta : float) -> void:
+	var idStr : String = str(playerId + 1)
+	
+	if !is_on_floor():
+		velocity.y -= fallSpeed * delta
+	
+	var input = Input.get_vector(
+		"move_left_p" + idStr,
+		"move_right_p" + idStr,
+		"move_up_p" + idStr,
+		"move_down_p" + idStr
+	)
+	
+	var direction = Vector3(input.x, 0.0, input.y)
+	
+	velocity.x = direction.x * moveSpeed * get_horizontal_speed_factor()
+	velocity.z = direction.z * moveSpeed * get_horizontal_speed_factor()
+	
+	if Input.is_action_just_pressed("jump_p" + idStr) and is_on_floor():
+		velocity.y = jumpVelocity
+	
+	move_and_slide()
+
+func get_horizontal_speed_factor() -> float:
+	var toReturn : float = 1.0
+	
+	if !is_on_floor():
+		toReturn *= 0.5
+	
+	return toReturn
